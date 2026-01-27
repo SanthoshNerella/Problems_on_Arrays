26ff94f3000b073f15a3270ab57ba1feb3cbcba1
santhu <santhoshnerella7@gmail.com>
2026-01-27 21:53:28 +0530

    Smallest Elent in Array

--- SmallestNumber.java
+++ SmallestNumber.java
@@ -1,5 +1,7 @@
+import java.util.Arrays;
 public class SmallestNumber {
     public static void main(String[] args) {
+                      // Optimal Approach
         int min_num = Integer.MAX_VALUE;
         int [] nums = {15,6,3,7,8,1};
         for(int num : nums){
@@ -8,5 +10,10 @@ public class SmallestNumber {
             }
         }
         System.out.println("the Smallest Value is: " + min_num);
+
+              /// Brute Force Approach
+    Arrays.sort(nums);
+    System.out.println("the Smallest Val is: " + nums[0]);  
     }  
+
 }
